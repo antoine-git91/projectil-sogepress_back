42f98af69943e26fea1bb106449a632defcea74c
Maxime AUBE <maxime.aube@protonmail.com>
2021-09-01 11:30:25 +0200

    update SupportPrint, suppression TypePrint

--- src/Entity/SupportPrint.php
+++ src/Entity/SupportPrint.php
@@ -28,12 +28,6 @@ class SupportPrint
      */
     private $commande;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=TypePrint::class)
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $type_print;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -59,18 +53,6 @@ class SupportPrint
     public function setCommande(Commande $commande): self
     {
         $this->commande = $commande;
-
-        return $this;
-    }
-
-    public function getTypePrint(): ?TypePrint
-    {
-        return $this->type_print;
-    }
-
-    public function setTypePrint(?TypePrint $type_print): self
-    {
-        $this->type_print = $type_print;
 
         return $this;
     }
