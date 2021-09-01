3059065a7a65a85a9ffa5bb6bcde6ffed7900ca3
Maxime AUBE <maxime.aube@protonmail.com>
2021-09-01 12:11:01 +0200

    run : symfony console doctrine:migrations:migrate

---
+++ migrations/Version20210901100631.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210901100631 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE support_print DROP FOREIGN KEY FK_E76C6ABEBDE1450');
+        $this->addSql('DROP TABLE type_print');
+        $this->addSql('DROP INDEX IDX_E76C6ABEBDE1450 ON support_print');
+        $this->addSql('ALTER TABLE support_print DROP type_print_id');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE type_print (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE support_print ADD type_print_id INT NOT NULL');
+        $this->addSql('ALTER TABLE support_print ADD CONSTRAINT FK_E76C6ABEBDE1450 FOREIGN KEY (type_print_id) REFERENCES type_print (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_E76C6ABEBDE1450 ON support_print (type_print_id)');
+    }
+}
