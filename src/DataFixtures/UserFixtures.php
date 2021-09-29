45e443b80c1673101476d34d72ce08276171726f
Maxime AUBE <maxime.aube@protonmail.com>
2021-09-29 11:20:33 +0200

    resolve merge conflict

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -7,7 +7,6 @@ use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -25,22 +24,21 @@ class UserFixtures extends Fixture implements DependentFixtureInterface, Fixture
     {
         // USER
         $test_user = new User();
-<<<<<<< HEAD
         $test_user->setNom('Projectil');
         $test_user->setPrenom('Sogepress');
         $test_user->setEmail('test@test.com');
         $test_user->setPassword($this->userPasswordHasher->hashPassword($test_user, 'vErySecure_1234'));
-=======
-        $test_user->setNom('Projectil2');
-        $test_user->setPrenom('Sogepress2');
-        $test_user->setEmail('adresse@mail.loc');
-        $test_user->setPassword('1234');
->>>>>>> BLG
         $test_user->setRoles(['ROLE_COMMERCIAL']);
         $test_user->setCreatedAt(new DateTimeImmutable('now'));
         $manager->persist($test_user);
-        $manager->flush();
         $this->addReference(self::USER_REFERENCE, $test_user); // ajout de la référence pour inclusion dans les autres fixtures
+
+//        $test_user->setNom('Projectil2');
+//        $test_user->setPrenom('Sogepress2');
+//        $test_user->setEmail('adresse@mail.loc');
+//        $test_user->setPassword('1234');
+
+        $manager->flush();
     }
 
     public function getDependencies() {
